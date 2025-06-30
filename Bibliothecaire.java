d706892d7deb3690d3d00c3972357da5d906ce7b
Nassiraouzzine <ouzzinenassira2032005@gmail.com>
2025-06-30 22:07:13 +0100

    Create Bibliothecaire.java

---
+++ Bibliothecaire.java
@@ -0,0 +1,7 @@
+package Gestion_Bibliotheque;
+
+public class Bibliothecaire extends Utilisateur {
+    public Bibliothecaire(String nom, String motDePasse) {
+        super(nom, motDePasse);
+    }
+ }
